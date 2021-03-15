2adcbede3f7eccab33f7e92db05b15a57c64f26d
zfy120 <ax152147@gmail.com>
2021-03-15 11:00:33 +0800

    add headers

--- jpeg.hpp
+++ jpeg.hpp
@@ -2,6 +2,12 @@
 
 #include <iostream>
 #include <fstream> 
+#include <vector> 
+#include <array> 
+#include <unordered_map>
+#include <utility> 
+#include <functional> 
+#include <boost/functional/hash.hpp>
 
 namespace myJpeg {
     void decode(const char *);
